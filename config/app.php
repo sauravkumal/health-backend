824e3f8f976e472a96d41596dd55e1d54a5636d7
Saurav Kumal <sk.sauravkumal@gmail.com>
2023-01-24 12:56:15 +0545

    fortify added

--- config/app.php
+++ config/app.php
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -185,6 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \App\Providers\FortifyServiceProvider::class,
 
         /*
          * Application Service Providers...
