fca2105cdccbf8338cca169ca36d2d69e1d0c5b6
Saurav Kumal <sk.sauravkumal@gmail.com>
2023-01-30 23:38:21 +0545

    added subcategory in category resource

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -9,16 +9,17 @@ class CategoryResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         return [
-            'id'=>$this->id,
-            'title'=>$this->title,
-            'position'=>$this->position,
-            'vendor_id' => $this->vendor_id
+            'id' => $this->id,
+            'title' => $this->title,
+            'position' => $this->position,
+            'vendor_id' => $this->vendor_id,
+            'subCategories' => SubCategoryResource::collection($this->subCategoies)
         ];
     }
 }
