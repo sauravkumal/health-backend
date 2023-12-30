6558d44201f920ad5399f1b5a3436730c92cdbd5
sauravkumal <sk.sauravkumal1@gmail.com>
2023-12-31 02:00:36 +0545

    migrate database

---
+++ app/Console/Commands/MigrateTelegramDb.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class MigrateTelegramDb extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:migrate-telegram-db';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Migrate telegram database';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $path = base_path('vendor/longman/telegram-bot/structure.sql');
+        $sql = file_get_contents($path);
+        $resp = DB::connection('telegram')->unprepared($sql);
+        if ($resp) {
+            $this->info('Database migrated successfully');
+        } else {
+            $this->error('Failed to migrate database');
+        }
+        return Command::SUCCESS;
+    }
+}
