da555c6ae1f3d9767d42e8a5ef78262ddec941cc
sauravkumal <sk.sauravkumal1@gmail.com>
2024-01-01 13:22:22 +0545

    test case

---
+++ tests/BotHandlerTestCase.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Longman\TelegramBot\Entities\Chat;
+use Longman\TelegramBot\Entities\Message;
+use Longman\TelegramBot\Entities\User;
+use Longman\TelegramBot\Request;
+use Mockery;
+
+class BotHandlerTestCase extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $seed = true;
+
+    protected Mockery\LegacyMockInterface|Mockery\MockInterface $client;
+    protected Mockery\LegacyMockInterface|Mockery\MockInterface $from;
+    protected Mockery\LegacyMockInterface|Mockery\MockInterface $chat;
+    protected Mockery\LegacyMockInterface|Mockery\MockInterface $message;
+    protected Response $sendMessageResponse;
+
+    protected function createHandlerTestBench(): void
+    {
+        Artisan::call('command:migrate-telegram-db');
+
+
+        $this->client = Mockery::mock(Client::class);
+
+
+        Request::setClient($this->client);
+
+        $id = fake()->numberBetween(10000, 10000);
+        $this->chat = Mockery::mock(Chat::class);
+        $this->chat->shouldReceive('getId')->andReturn($id);
+        $this->from = Mockery::mock(User::class);
+        $this->from->shouldReceive('getId')->andReturn($id);
+        $this->from->shouldReceive('getFirstName')->andReturn(fake()->firstName);
+        $this->from->shouldReceive('getLastName')->andReturn(fake()->lastName);
+        $this->from->shouldReceive('getUsername')->andReturn(fake()->userName);
+
+        DB::table('user')
+            ->insert([
+                'id' => $id,
+                'first_name' => fake()->name
+            ]);
+
+        DB::table('chat')
+            ->insert([
+                'id' => $id,
+                'type' => 'private'
+            ]);
+
+
+        $this->message = Mockery::mock(Message::class);
+        $this->message->shouldReceive('getFrom')->andReturn($this->from);
+        $this->message->shouldReceive('getChat')->andReturn($this->chat);
+
+
+        $respJson = [
+            "ok" => true,
+            "result" => [
+                "message_id" => fake()->randomNumber(),
+                "from" => [
+                    "id" => $id,
+                    "is_bot" => true,
+                    "first_name" => "Health Tracker",
+                    "username" => "health_tracker_saurav_bot"
+                ],
+                "chat" => [
+                    "id" => $id,
+                    "first_name" => fake()->firstName,
+                    "last_name" => fake()->lastName,
+                    "username" => fake()->userName,
+                    "type" => "private"
+                ],
+                "date" => fake()->unixTime,
+                "text" => "👤Enter your name"
+            ]
+        ];
+
+
+        $this->sendMessageResponse = new Response(200,
+            ['Content-Type' => 'application/json'],
+            json_encode($respJson));
+
+        $this->client->shouldReceive('post')->with(
+            Mockery::on(function ($arg) {
+                return Str::contains($arg, 'sendMessage');
+            }), Mockery::on(fn($args) => true))->andReturn($this->sendMessageResponse);
+    }
+}
--- tests/Unit/BotHandlers/NewUserHandlerTest.php
+++ tests/Unit/BotHandlers/NewUserHandlerTest.php
@@ -2,20 +2,35 @@
 
 namespace Tests\Unit\BotHandlers;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Artisan;
-use Tests\TestCase;
+use App\Bot\Commands\User\StartCommand;
+use App\Bot\Handlers\NewUserHandler;
+use Illuminate\Support\Str;
+use Longman\TelegramBot\Exception\TelegramException;
+use Mockery;
+use Tests\BotHandlerTestCase;
 
-class NewUserHandlerTest extends TestCase
+class NewUserHandlerTest extends BotHandlerTestCase
 {
 
-    use RefreshDatabase;
-
-    protected $seed = true;
-
+    /**
+     * @throws TelegramException
+     */
     public function test_if_new_user_handler_is_working()
     {
-        Artisan::call('command:migrate-telegram-db');
+        $this->createHandlerTestBench();
+
+        $command = Mockery::mock(StartCommand::class);
+        $command->shouldReceive('getMessage')->andReturn($this->message);
+
+        $inputs = ['', 'Saurav Kumal', '2000-12-15', 'male'];
+        $this->message->shouldReceive('getText')
+            ->andReturn(...$inputs);
+
+        foreach ($inputs as $input) {
+            $handler = new NewUserHandler($command);
+            $handler->handle();
+        }
+
         $this->assertTrue(true);
     }
 
